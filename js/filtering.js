9e5c9f2aa32fed7c0184579acd0accce070366df
adamburich <adamburich@gmail.com>
2023-03-18 22:54:08 -0400

    Completely re-wrote ApplyFilters
    It works now, logic is not flawed (it must have been the last time and I can't figure out in what way).
    
    Going to pull out the bigger chunks and give them their own functions.

--- js/filtering.js
+++ js/filtering.js
@@ -16,15 +16,15 @@ function Setup() {
     remove_filters.addEventListener("click", ClearFilters);
     mode_select.addEventListener("change", ModeChange);
 
-    var main = document.getElementsByTagName("main")[0];
+    let main = document.getElementsByTagName("main")[0];
     
-    var scroll = document.querySelector("#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area > div.simplebar-scroll-content")
+    let scroll = document.querySelector("#root > div > div.Layout-sc-1xcs6mc-0.kBprba > div > main > div.root-scrollable.scrollable-area > div.simplebar-scroll-content")
     scroll.scrollTo(0, scroll.scrollHeight);
     setTimeout(function(){
         scroll.scrollTo(0, 0);
     }, 500)
     setTimeout(function () {
-        var following = document.querySelectorAll("div.live-channel-card");
+        let following = document.querySelectorAll("div.live-channel-card");
         //console.log(following);
         main.addEventListener("click", function (event) {
             for (let i = 0; i < following.length; i++) {
@@ -34,8 +34,8 @@ function Setup() {
             }
         })
 
-        var games = GetGameList();
-        var field = document.getElementById("filter_name");
+        let games = GetGameList();
+        let field = document.getElementById("filter_name");
         autocomplete(field, games);
         AdjustCookies();
     }, 1000)
@@ -43,8 +43,8 @@ function Setup() {
 }
 
 function GetGameList() {
-    var games = document.querySelectorAll('[data-test-selector="GameLink"]');
-    var gameList = [];
+    let games = document.querySelectorAll('[data-test-selector="GameLink"]');
+    let gameList = [];
     for (let i = 0; i < games.length; i++) {
         //console.log(games[i].innerText)
         if (gameList.indexOf(games[i].innerText) == -1) {
@@ -54,40 +54,108 @@ function GetGameList() {
     return gameList;
 }
 
+// function ApplyFilters(mode_param) {
+//     let following = document.querySelectorAll("div.live-channel-card");
+//     //let hide = mode === "hide";
+//     let hide = mode_param === "hide";
+//     let select_mode = document.getElementById("mode");
+//     if(select_mode != null){
+//         document.getElementById("mode").value = mode_param;
+//     }
+//     for (let i = 0; i < following.length; i++) {
+//         if(filterList.length > 0){
+//             if (hide) {
+//                 for (let j = 0; j < filterList.length; j++) {
+//                     let inner = following[i].innerHTML.toLowerCase();
+//                     if (inner.indexOf(filterList[j].toLowerCase()) != -1) {
+//                         following[i].parentNode.style.display = "none";
+//                     }else{
+//                         following[i].parentNode.style.display = "block";
+//                     }
+//                 }
+//             } else {
+//                 following[i].parentNode.style.display = "none";
+//                 for (let j = 0; j < filterList.length; j++) {
+//                     let inner = following[i].innerHTML.toLowerCase();
+//                     if (inner.indexOf(filterList[j].toLowerCase()) != -1) {
+//                         following[i].parentNode.style.display = "block";
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+function ApplyHideFilter(follow_list, filt_list){
+
+}
+
 function ApplyFilters(mode_param) {
-    var following = document.querySelectorAll("div.live-channel-card");
-    //var hide = mode === "hide";
-    var hide = mode_param === "hide";
-    var select_mode = document.getElementById("mode");
+    let following = document.querySelectorAll("div.live-channel-card");
+    let hide_me = [];
+    let show_me = [];
+    //let hide = mode === "hide";
+    let hide = mode_param === "hide";
+    let select_mode = document.getElementById("mode");
     if(select_mode != null){
         document.getElementById("mode").value = mode_param;
     }
-    for (let i = 0; i < following.length; i++) {
-        if(filterList.length > 0){
-            if (hide) {
-                for (let j = 0; j < filterList.length; j++) {
-                    var inner = following[i].innerHTML.toLowerCase();
-                    if (inner.indexOf(filterList[j].toLowerCase()) != -1) {
-                        following[i].parentNode.style.display = "none";
-                    }else{
-                        following[i].parentNode.style.display = "block";
+    
+    for(let i = 0; i < following.length; i++){
+        let inner = following[i].parentElement.innerText.toLowerCase();
+        if(hide){
+            console.log("We should only see this message when dropdown is set to Hide")
+            for(let j = 0; j < filterList.length; j++){
+                let thisFilter = filterList[j].toLowerCase();
+                if(inner.includes(thisFilter)){
+                    hide_me.push(following[i].parentElement);
+                    if(show_me.includes(following[i].parentElement)){
+                        let ind = show_me.indexOf(following[i].parentElement);
+                        show_me.splice(ind, 1, );
+                    }
+                }else{
+                    console.log(inner)
+                    console.log(thisFilter)
+                    if(!hide_me.includes(following[i].parentElement)){
+                        if(!show_me.includes(following[i].parentElement)){
+                            show_me.push(following[i].parentElement);
+                        }
                     }
                 }
-            } else {
-                following[i].parentNode.style.display = "none";
-                for (let j = 0; j < filterList.length; j++) {
-                    var inner = following[i].innerHTML.toLowerCase();
-                    if (inner.indexOf(filterList[j].toLowerCase()) != -1) {
-                        following[i].parentNode.style.display = "block";
+            }
+        }else{
+            console.log("We should only see this message when dropdown is set to Show")
+            for(let j = 0; j < filterList.length; j++){
+                let thisFilter = filterList[j].toLowerCase();
+                if(inner.includes(thisFilter)){
+                    show_me.push(following[i].parentElement);
+                    if(hide_me.includes(following[i].parentElement)){
+                        let ind = hide_me.indexOf(following[i].parentElement);
+                        hide_me.splice(ind, 1, );
+                    }
+                }else{
+                    if(!show_me.includes(following[i].parentElement)){
+                        if(!hide_me.includes(following[i].parentElement)){
+                            hide_me.push(following[i].parentElement);
+                        }
                     }
                 }
             }
         }
     }
+    // console.log("Following returned", following.length, "elements")
+    // console.log("Hide_me contains", hide_me.length, "elements")
+    // console.log("Show_me contains", show_me.length, "elements")
+    for(let i = 0; i < hide_me.length; i++){
+        hide_me[i].style.display = "none";
+    }
+    for(let i = 0; i < show_me.length; i++){
+        show_me[i].style.display = "block";
+    }
 }
 
 function AddFilter() {
-    var filter_val = document.getElementById("filter_name").value;
+    let filter_val = document.getElementById("filter_name").value;
     document.getElementById("filter_name").value = "";
     filterList[filterList.length] = filter_val;
 
@@ -100,8 +168,8 @@ function ModeChange() {
 }
 
 function ClearFilters() {
-    var following = document.querySelector("#following-page-main-content > div:nth-child(1) > div > div").childNodes;
-    var filter_div = document.getElementById("filter-container");
+    let following = document.querySelector("#following-page-main-content > div:nth-child(1) > div > div").childNodes;
+    let filter_div = document.getElementById("filter-container");
     while (filter_div.firstChild) {
         filter_div.removeChild(filter_div.firstChild);
     }
@@ -116,7 +184,7 @@ async function AdjustCookies() {
     if (retrieved) {
         (async () => {
             const response = await chrome.runtime.sendMessage({ filters: filterList.join(','), filterMode: mode });
-            var filters = response.cookieFilters.split("|")[0];
+            let filters = response.cookieFilters.split("|")[0];
             if (filters.length > 1) {
                 filterList = filters.split(",");
             }
@@ -125,7 +193,7 @@ async function AdjustCookies() {
     } else {
         (async () => {
             const response = await chrome.runtime.sendMessage({ filters: null, filterMode: null });
-            var filters = response.cookieFilters.split("|")[0];
+            let filters = response.cookieFilters.split("|")[0];
             if (filters.length > 1) {
                 filterList = filters.split(",");
             }
@@ -141,9 +209,9 @@ async function AdjustCookies() {
 }
 
 function AddFilterUIObjects() {
-    var filterContainer = document.getElementById("filter-container");
+    let filterContainer = document.getElementById("filter-container");
     for (let i = 0; i < filterList.length; i++) {
-        var skip = false;
+        let skip = false;
         if(filterContainer != null){
             for (let j = 0; j < filterContainer.childNodes.length; j++) {
                 if (filterContainer.childNodes[j].innerText === filterList[i]) {
@@ -151,7 +219,7 @@ function AddFilterUIObjects() {
                 }
             }
             if (!skip) {
-                var to_add = document.createElement("div");
+                let to_add = document.createElement("div");
                 to_add.innerText = filterList[i];
                 to_add.setAttribute("class", "filter");
     
